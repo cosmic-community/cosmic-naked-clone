6df970f734102c2ee3c3776df5b8809058faecdd
Cosmic AI <support@cosmicjs.com>
2025-07-09 13:08:40 -0700

    Update favicon to use 🫣 emoji encoded as data URL

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: 'Cosmic Naked Clone',
   description: 'A modern web agency showcase built with Next.js and Cosmic CMS',
   icons: {
-    icon: '/favicon.png',
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🫣</text></svg>",
   },
 }
 
